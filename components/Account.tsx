dd91a111ac9c893a4db3b11b7d8f717077caf4f3
Brenet <jrm.devweb@gmail.com>
2024-11-24 12:55:33 +0100

    init

--- components/Account.tsx
+++ components/Account.tsx
@@ -7,8 +7,6 @@ import { Session } from '@supabase/supabase-js'
 export default function Account({ session }: { session: Session }) {
     const [loading, setLoading] = useState(true)
     const [username, setUsername] = useState('')
-    const [website, setWebsite] = useState('')
-    const [avatarUrl, setAvatarUrl] = useState('')
 
     useEffect(() => {
         if (session) getProfile()
@@ -21,7 +19,7 @@ export default function Account({ session }: { session: Session }) {
 
             const { data, error, status } = await supabase
                 .from('profiles')
-                .select(`username, website, avatar_url`)
+                .select(`username, avatar_url`)
                 .eq('id', session?.user.id)
                 .single()
             if (error && status !== 406) {
@@ -30,8 +28,6 @@ export default function Account({ session }: { session: Session }) {
 
             if (data) {
                 setUsername(data.username)
-                setWebsite(data.website)
-                setAvatarUrl(data.avatar_url)
             }
         } catch (error) {
             if (error instanceof Error) {
@@ -44,11 +40,9 @@ export default function Account({ session }: { session: Session }) {
 
     async function updateProfile({
                                      username,
-                                     website,
                                      avatar_url,
                                  }: {
         username: string
-        website: string
         avatar_url: string
     }) {
         try {
@@ -58,7 +52,6 @@ export default function Account({ session }: { session: Session }) {
             const updates = {
                 id: session?.user.id,
                 username,
-                website,
                 avatar_url,
                 updated_at: new Date(),
             }
@@ -84,17 +77,6 @@ export default function Account({ session }: { session: Session }) {
             </View>
             <View style={styles.verticallySpaced}>
                 <Input label="Username" value={username || ''} onChangeText={(text) => setUsername(text)} />
-            </View>
-            <View style={styles.verticallySpaced}>
-                <Input label="Website" value={website || ''} onChangeText={(text) => setWebsite(text)} />
-            </View>
-
-            <View style={[styles.verticallySpaced, styles.mt20]}>
-                <Button
-                    title={loading ? 'Loading ...' : 'Update'}
-                    onPress={() => updateProfile({ username, website, avatar_url: avatarUrl })}
-                    disabled={loading}
-                />
             </View>
 
             <View style={styles.verticallySpaced}>
